2b5e69f2a43bafe28dc6bd1ec74c6eeb9fb425c9
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-22 23:17:50 +0530

    Variables Part 1

---
+++ variables.js
@@ -0,0 +1,9 @@
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
+//undefined  is the default value of uninitialized variable.
+secondCharacter = "Test";
+//we can still assign value to an uninitialized variable.
+console.log(secondCharacter);
