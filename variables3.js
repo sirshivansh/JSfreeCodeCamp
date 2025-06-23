a230951f9f270d414a1226b6be8b8a2712f55ece
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-23 15:07:33 +0530

    Variables in JS : 3

---
+++ variables3.js
@@ -0,0 +1,11 @@
+// variables which are declared with "const" keyword cannot be reassigned like those with "let" keyword
+// "const" variables cannot change their values
+// a "const" variable cannot be uninitialized
+
+const character = "Hello";
+const count = 8;
+const rows = [];
+
+console.log(character);
+console.log(count);
+console.log(rows);
