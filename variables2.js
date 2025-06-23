ba368815f75c8a0f7efb2dada1da8af8dbda861d
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-23 11:31:41 +0530

    Variables in JavaScript : 2

---
+++ variables2.js
@@ -0,0 +1,15 @@
+//uninitialized variables have a default primitive value = undefined.
+let uninitialized;
+console.log(uninitialized);
+//we can assign value to a variable that is undefined.
+uninitialized = "defined";
+console.log(uninitialized);
+
+// undefined is also a type of datatype in JavaScript
+console.log(undefined);
+
+// assigning value of a variabke to another variable.
+let first = "One";
+let second = "Two";
+second = first;
+console.log(second);
