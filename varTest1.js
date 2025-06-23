406ae1c315e63365441930c8923af8756cd5fba4
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-23 11:40:06 +0530

    Variable Test 1

---
+++ varTest1.js
@@ -0,0 +1,10 @@
+/* Problem Statement :
+Use the let keyword to declare a profession variable and an age variable. 
+Initialize profession with the string "teacher", but do not initialize age with any value.
+Log both of your variables to the console to see the results. */
+
+let profession;
+let age;
+profession = "teacher";
+console.log(profession);
+console.log(age);
