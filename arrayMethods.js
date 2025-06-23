5ff28729c01b97179b4caa88a546406e1656f8af
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-23 14:52:15 +0530

    Array Methods

---
+++ arrayMethods.js
@@ -0,0 +1,11 @@
+// Arrays have their own methods!
+let arr = ["Rick","Nick","Pick"];
+console.log(arr); // arr is displayed
+
+let pushed = arr.push("Zig"); // .push() method
+console.log(pushed); // .push() returns new length of the array (neumerical value)
+console.log(arr); // updated array is displayed
+
+let popped = arr.pop(); // .pop() method
+console.log(popped); // popped element is displayed
+console.log(arr); // updated array is displayed
