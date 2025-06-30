1ab5544b83a3c44a1992e328618a6e35b0810e21
Shivansh Mishra <shivnsh01@gmail.com>
2025-06-30 15:42:13 +0530

    getAverage() function

---
+++ Review JS Fundamentals by Building a Gradebook app/gradebook1.js
@@ -0,0 +1,15 @@
+function getAverage(scores) {
+  let sum = 0;
+  for(let i=0; i<scores.length; i++){
+    sum += scores[i];
+  }
+
+  if(scores.length === 0){
+    return 0;
+  }
+
+  return sum/scores.length;
+}
+
+console.log(getAverage([92, 88, 12, 77, 57, 100, 67, 38, 97, 89]));
+console.log(getAverage([45, 87, 98, 100, 86, 94, 67, 88, 94, 95]));
